0ecd3f5b1c0d9ccdcbfc2b74a5ee393b54b586c1
id110099122 <id110099122@gmail.com>
2012-02-14 01:31:43 +0000

    Making stdFramework Structure.

--- stdCore.php
+++ stdCore.php
@@ -1,10 +1,11 @@
 <?
-	/**
-	 * @name StandardFramework Core
-	 * @author DENFER
-	 * @version 1.0.0
-	 * @copyright DENFER STUDIO
-	 */
+
+/**
+ * @name StandardFramework Core
+ * @author DENFER
+ * @version 1.0.0
+ * @copyright DENFER STUDIO
+ */
 	
 abstract class stdCore
 {
