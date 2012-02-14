cf89f3e34b13fe9f2b41700e4abe6d8006139670
id110099122@gmail.com <id110099122@gmail.com@0c0a8ab9-7916-9e06-d460-ca4c9cb33b65>
2012-02-14 05:08:29 +0000

    Added Engine

---
+++ trunk/Core/Screen.php
@@ -0,0 +1,14 @@
+<?
+
+/**
+ * @name StandardFramework Screen
+ * @author DENFER
+ * @version 1.0.0
+ * @copyright DENFER STUDIO
+ */
+
+class Screen
+{
+	public $Width = 800;
+	public $Height = 600;
+}
